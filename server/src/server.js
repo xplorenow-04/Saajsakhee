be425e9e7a7db93c4a4e1c75fd4d30afb5396c9f
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-07-01 19:18:18 +0530

    fdfds

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://saajsakhee.vercel.app"
 ].filter(Boolean);
 
 app.use(cors({
